73175060d86d2912b9b00ed3822c9836edb6143b
santosgonz <130101203+santosgonz@users.noreply.github.com>
2023-11-29 08:43:17 -0800

    Add files via upload

---
+++ reservation.js
@@ -0,0 +1,66 @@
+/** Reservation for Lunchly */
+
+const moment = require("moment");
+
+const db = require("../db");
+
+
+/** A reservation for a party */
+
+class Reservation {
+  constructor({id, customerId, numGuests, startAt, notes}) {
+    this.id = id;
+    this.customerId = customerId;
+    this.numGuests = numGuests;
+    this.startAt = startAt;
+    this.notes = notes;
+  }
+
+  /** formatter for startAt */
+
+  getformattedStartAt() {
+    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+  }
+
+  /** given a customer id, find their reservations. */
+
+  static async getReservationsForCustomer(customerId) {
+    const results = await db.query(
+          `SELECT id, 
+           customer_id AS "customerId", 
+           num_guests AS "numGuests", 
+           start_at AS "startAt", 
+           notes AS "notes"
+         FROM reservations 
+         WHERE customer_id = $1`,
+        [customerId]
+    );
+
+    return results.rows.map(row => new Reservation(row));
+  }
+
+  async save() {
+    if (this.id === undefined) {
+      // console.log(this.numGuests)
+      // console.log(this.notes)
+      // console.log(this.startAt)
+      // console.log(this.customerId)
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id,   
+        start_at, 
+        num_guests,
+        notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customerId, this.startAt, this.numGuests,  this.notes]
+        
+      );
+      this.id = result.rows[0].id;
+    } else {
+      return("hi")
+    }
+  }
+}
+
+
+module.exports = Reservation;
